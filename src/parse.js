16bbee9d8105b1e52b4ead14d048b5e7cfec7a60
evio <evio@vip.qq.com>
2019-03-10 17:31:18 +0800

    add mixin functional

--- src/parse.js
+++ src/parse.js
@@ -54,7 +54,7 @@ export default class Parser {
       })
     });
     mixins.forEach(context => {
-      this.ContextEach(context, (key, mixin) => Vue.mixin(mixin));
+      this.ContextEach(context, (key, mixin) => Vue.mixin(typeof directive === 'function' ? mixin(app) : mixin));
     });
   }
 
